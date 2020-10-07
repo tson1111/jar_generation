db5b6c4e4e6f56fa5e9ebaccfefccb33fc990f8d
tson1111 <757495600@qq.com>
2020-10-07 12:20:44 -0500

    feat: script done.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # jar_generation
-Generate jars for query workloads
+Generate jars for query workloads.
+
+1. Install JDK and JRE.
+
+```
+sudo apt update
+sudo apt install openjdk-8-jre-headless openjdk-8-jdk-headless
+```
+
+2. Compile Spark.
+
+```
+cd dataloading
+./build/sbt package
+cd ../newspark
+./build/sbt package
+```
+
+3. Generate jars.
+
+```
+./test.sh
+```
+
+4. Git commit.
---
+++ workload/review/uni/q153.scala
@@ -0,0 +1,67 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.spark.examples.sql
+
+import java.util.Properties
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.internal.SQLConf
+
+object SQLDataSourceExample {
+
+  case class Person(name: String, age: Long)
+
+  def main(args: Array[String]) {
+
+    val spark = SparkSession
+      .builder()
+      .appName("Spark SQL data sources example")
+      .config("spark.sql.parquet.enableVectorizedReader", "false")
+      .config(SQLConf.QUERY_BIT_VECTOR.key, "/local/cong/bv")
+      .getOrCreate()
+
+    q1(spark)
+    spark.stop()
+  }
+
+  private def q1(spark: SparkSession): Unit = {
+    import spark.implicits._
+    val time1 = System.currentTimeMillis()
+    val df = spark.read
+      .parquet("/local/cong/review.pqt")
+
+    val res = df
+      .filter($"funny"===11)
+      .filter($"cool"===13)
+      .filter($"useful"===13)
+      .filter($"cool"===25)
+      .filter($"funny"===29)
+      .filter($"funny"===30)
+      .filter($"funny"===33)
+      .filter($"useful"===34)
+      .filter($"useful"===41)
+      .filter($"funny"===54)
+      .count()
+    // scalastyle:off println
+    System.out.println(res)
+    val time2 = System.currentTimeMillis()
+    System.out.println(time2-time1)
+    // scalastyle:on println
+  }
+}
+
